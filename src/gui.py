2cf753852d448a22d214d0495f717dea13d2687b
Andrew <andrewk2017@gmail.com>
2024-05-15 16:43:17 -0600

    Adding images for pieces in the gui and making the background checkered

--- src/gui.py
+++ src/gui.py
@@ -19,23 +19,39 @@ class ChessBoard:
         self.first_click = None
         self.mainframe = ttk.Frame(self.root)
         self.mainframe.grid(column=0, row=0)
+        self.load_images()
         self.create_board()
 
+    # function to load in all the piece images for efficiency
+    def load_images(self):
+        # make image dictionary to store piece images
+        self.images = {}
+        for name in ['K', 'Q', 'R', 'N', 'B', 'P']:
+            self.images[name + 'w'] = tk.PhotoImage(file=f'resources/{name}w.png')
+            self.images[name + 'b'] = tk.PhotoImage(file=f'resources/{name}b.png')
+        # also add empty image to images dictionary
+        self.images['_'] = tk.PhotoImage(width=135, height=135)
+    # function that creates all the button widgets and puts them in the correct spot
+    # with the correct command function
     def create_board(self):
+        color1 = "#F5D7A4"  # Light brown
+        color2 = "#694507"  # Dark brown
         for nrow in range(8):
             for ncol in range(8):
+                # colors for checkerboard pattern
+                color = color1 if (nrow + ncol) % 2 == 0 else color2
+                img = self.images['_']
                 for i in range(16):
                     if 7-nrow == self.wc[i][0] and ncol == self.wc[i][1]:
-                        txt = self.names[i] + 'w'
+                        img = self.images[self.names[i] + 'w']
                         break
                     elif 7-nrow == self.bc[i][0] and ncol == self.bc[i][1]:
-                        txt = self.names[i] + 'b'
+                        img = self.images[self.names[i] + 'b']
                         break
-                    else:
-                        txt = "__"
-                btn = tk.Button(self.mainframe, text = txt)
+                btn = tk.Button(self.mainframe, image=img, bg=color)
                 btn.grid(row=nrow, column=ncol)
                 btn.config(command=partial(self.handle_click, ncol, 7-nrow, btn))
+                btn.image = img # make reference, avoid garbage collection
 
     def handle_click(self, col, row, button):
         pos = (row, col)
@@ -57,10 +73,10 @@ class ChessBoard:
                 # updating piece locations
                 for i in range(16):
                     if 7-row == self.wc[i][0] and col == self.wc[i][1]:
-                        child.config(text = self.names[i] + 'w')
+                        child.config(image=self.images[self.names[i] + 'w'])
                         break
                     elif 7-row == self.bc[i][0] and col == self.bc[i][1]:
-                        child.config(text = self.names[i] + 'b')
+                        child.config(image=self.images[self.names[i] + 'b'])
                         break
                     else:
-                        child.config(text = '__')
+                        child.config(image=self.images['_'])
