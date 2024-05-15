fa0d8e8c7b0c32e2c5212724a76823d2c33a887b
Andrew <andrewk2017@gmail.com>
2024-05-15 14:51:14 -0600

    Adding en passant move

--- src/game.py
+++ src/game.py
@@ -12,31 +12,26 @@ class Game():
 
     def play(self):
         print("Game starting! It is white to move.")
-        print("-----------------------------------------------")
+        print("-----------------------------------------------------------------")
         root = Tk()
         ChessBoard(root, self.make_move, self.board.w_coords, self.board.b_coords)
         root.mainloop()
 
     # ChessBoard class will call this when it gets two clicks input
     def make_move(self, sq1, sq2):
-        colors = {0: "white", 1: "black"}
+        colors = {0: "White", 1: "Black"}
         if self.board.valid_move([sq1, sq2], self.turn):
             tmp_board = copy.deepcopy(self.board)
-            print("checking apply move on tmp board...")
+            print("Checking apply move on tmp board...")
             tmp_board.apply_move([sq1, sq2], self.turn)
             # check opponents attacked squares for check
             if(tmp_board.king_safety(not self.turn)):
-                print("king still safe, still applying...")
+                print("King still safe, still applying...")
                 self.board.apply_move([sq1, sq2], self.turn)
                 self.board.unenpassant(not self.turn)
                 self.turn = not self.turn 
-                print("move applied, it is now {}'s turn.".format(colors[self.turn]))
-                print("-----------------------------------------------")
+                print("Move applied, it is now {}'s turn.".format(colors[self.turn]))
+                print("-----------------------------------------------------------------")
         else:
             print("*** Invalid move, try again. ***")
-        # reset move flags
-        self.board.set_enpassant_kingside(0)
-        self.board.set_enpassant_queenside(0)
-        self.board.set_caslte_kingside(0)
-        self.board.set_castle_queenside(0)
         return self.board.w_coords, self.board.b_coords # to update ChessBoard
