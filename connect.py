3b24f7a6e314d8b2f089626e7c179c91a4815ea9
Andrew <andrewk2017@gmail.com>
2024-06-07 16:11:46 -0600

    Fixing environment variables in compose.yml

--- connect.py
+++ connect.py
@@ -2,10 +2,6 @@ import psycopg2, os
 
 def connect():
     try:
-        print("POSTGRES_HOST:", os.getenv('POSTGRES_HOST', 'db'))
-        print("POSTGRES_DB:", os.getenv('POSTGRES_DB'))
-        print("POSTGRES_USER:", os.getenv('POSTGRES_USER'))
-        print("POSTGRES_PASSWORD:", os.getenv('POSTGRES_PASSWORD'))
         conn = psycopg2.connect(host=os.getenv('POSTGRES_HOST', 'db'),
                                 database=os.getenv('POSTGRES_DB'),
                                 user=os.getenv('POSTGRES_USER'),
