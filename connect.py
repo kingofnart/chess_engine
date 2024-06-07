15083c2f27afe6e1c3448ca5848e2633aaab4387
Andrew <andrewk2017@gmail.com>
2024-06-07 15:57:42 -0600

    Testing if github set correct env vars

--- connect.py
+++ connect.py
@@ -1,10 +1,15 @@
-import psycopg2
-from config import load_config
+import psycopg2, os
 
 def connect():
-    config = load_config()
     try:
-        conn = psycopg2.connect(**config)
+        print("POSTGRES_HOST:", os.getenv('POSTGRES_HOST', 'db'))
+        print("POSTGRES_DB:", os.getenv('POSTGRES_DB'))
+        print("POSTGRES_USER:", os.getenv('POSTGRES_USER'))
+        print("POSTGRES_PASSWORD:", os.getenv('POSTGRES_PASSWORD'))
+        conn = psycopg2.connect(host=os.getenv('POSTGRES_HOST', 'db'),
+                                database=os.getenv('POSTGRES_DB'),
+                                user=os.getenv('POSTGRES_USER'),
+                                password=os.getenv('POSTGRES_PASSWORD'))
         print('Connected to the PostgreSQL server.')
         return conn
     except (psycopg2.DatabaseError, Exception) as error:
--- start_and_test.sh
+++ start_and_test.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Print environment variables for debugging
+echo "POSTGRES_DB: $POSTGRES_DB"
+echo "POSTGRES_USER: $POSTGRES_USER"
+echo "POSTGRES_PASSWORD: $POSTGRES_PASSWORD"
+echo "DATABASE_URL: $DATABASE_URL"
+echo "CHESS_DB_SECRET_KEY: $CHESS_DB_SECRET_KEY"
+
 python app.py &
 FLASK_PID=$!
 
