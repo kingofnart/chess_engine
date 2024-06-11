67fb9ee89b02e5db960f6d6c52a742334fb8646b
Andrew <andrewk2017@gmail.com>
2024-06-11 16:03:18 -0600

    Added css to style game_history.html

--- init_db.py
+++ init_db.py
@@ -24,7 +24,7 @@ def initialize_database():
             game_id SERIAL PRIMARY KEY,
             user_id INTEGER REFERENCES users(user_id),
             game_history TEXT NOT NULL,
-            game_time TIMESTAMP NOT NULL
+            game_time TIMESTAMPTZ NOT NULL
         );
                     
         CREATE INDEX idx_games_user_id ON games (user_id);
