d919dd74a0e30485c429205626b789ed8727feb3
Andrew <andrewk2017@gmail.com>
2024-06-16 13:51:15 -0600

    Running chrome in headless mode

--- tests/test_chess_app.py
+++ tests/test_chess_app.py
@@ -15,7 +15,7 @@ from flask import session
 @pytest.fixture(scope="module")
 def driver():
     chrome_options = Options()
-    #chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
